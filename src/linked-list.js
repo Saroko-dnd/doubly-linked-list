c9982c7c39c0a29ba0f4d0a8883d790d6d5d85ea
saroko-dnd <isaroko@mail.ru>
2017-09-26 00:15:15 +0300

    Added implementation for head(), tail() and append(data).

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,13 +1,28 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.length = 0;
+    }
 
-    append(data) {}
+    append(data) {
+        if(this.length){
+            this._tail.next = new Node(data, this._tail, null);
+            this._tail = this._tail.next;
+        }else{
+            this._tail = new Node(data);
+            this._head = this._tail;
+        }
+        this.length += 1;
+    }
 
-    head() {}
+    head() {
+        return (this.length) ? this._head.data : null;
+    }
 
-    tail() {}
+    tail() {
+        return (this.length) ? this._tail.data : null;
+    }
 
     at(index) {}
 
