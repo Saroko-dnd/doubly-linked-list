adeed184322d602d9a85101f130c98b9effd22e4
saroko-dnd <isaroko@mail.ru>
2017-09-26 06:00:05 +0300

    Fixed: insertAt(index, data). Added: deleteAt(index)

--- src/linked-list.js
+++ src/linked-list.js
@@ -7,13 +7,13 @@ class LinkedList {
 
     append(data) {
         if(this.length){
-            this._tail.next = new Node(data, this._tail, null);
+            this._tail.next = new Node(data, this._tail);
             this._tail = this._tail.next;
         }else{
             this._tail = new Node(data);
             this._head = this._tail;
         }
-        this.length += 1;
+        ++this.length;
     }
 
     head() {
@@ -29,15 +29,18 @@ class LinkedList {
     }
 
     insertAt(index, data) {
-        if (this.length){
+        if (index === this.length){
+            this.append(data);
+        }else{
             let foundNode = this._findNode(index);
             const newNode = new Node(data, foundNode.prev, foundNode);
-            if (foundNode.prev != null){
+            if (foundNode.prev){
                 foundNode.prev.next = newNode;
+            }else{
+                this._head = newNode;
             }
             foundNode.prev = newNode;
-        }else{
-            this.append(data);
+            ++this.length;
         }
     }
 
@@ -60,7 +63,22 @@ class LinkedList {
     }
 
     deleteAt(index) {
-        let foundNode = this._findNode(index); 
+        if (this.length === 1){
+            this.clear();
+        }else{
+            let foundNode = this._findNode(index);
+            if (!foundNode.next){
+                this._tail = this._tail.prev;
+                this._tail.next = null;
+            }else if (!foundNode.prev){
+                this._head = this._head.next;
+                this._head.prev = null;
+            }else{
+                foundNode.prev.next = foundNode.next;
+                foundNode.next.prev = foundNode.prev;             
+            }
+            --this.length;  
+        }
     }
 
     reverse() {}
